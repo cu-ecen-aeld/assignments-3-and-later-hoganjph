3b6e9064de4a52df44111b09f30f03e3f1e46bce
John Hogan <jhogan41@gmail.com>
2024-12-03 22:00:46 -0800

    changes to finder-app to work with assignment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,6 +10,9 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
 
+echo "Changing directory to $(dirname $0)"
+cd `dirname $0`
+
 if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
@@ -53,13 +56,14 @@ for i in $( seq 1 $NUMFILES)
 do
         if [ $assignment != 'assignment1' ]
         then
-                ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+                writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
         else
                 ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
         fi
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+echo $OUTPUTSTRING > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
